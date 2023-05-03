ef270f8e2acd7ff3df911d912515d4a6d9b6ce11
Martin Dobrev <martindobrev87@gmail.com>
2023-05-03 08:32:00 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,155 @@
+import {
+  Client,
+  PrivateKey,
+  AccountId,
+  ContractCreateFlow,
+  ContractUpdateTransaction,
+  ContractExecuteTransaction,
+  ContractFunctionParameters,
+  ContractInfoQuery,
+  AccountAllowanceApproveTransaction,
+} from "@hashgraph/sdk";
+import axios from "axios";
+import * as dotenv from "dotenv";
+import WHBAR from "./abi/WHBAR.json" assert { type: "json" };
+
+dotenv.config();
+console.clear();
+
+const bytecode = WHBAR.bytecode;
+
+// Functions
+const getClient = () => {
+  //Grab your Hedera testnet account ID and private key from your .env file
+  const myAccountId = AccountId.fromString(process.env.MY_ACCOUNT_ID);
+  // const myPrivateKey = PrivateKey.fromString(process.env.MY_PRIVATE_KEY);
+  const myPrivateKey = PrivateKey.fromStringECDSA(process.env.MY_PRIVATE_KEY);
+  // const stakeAccount = "0.0.7027";
+  const stakeAccount = AccountId.fromString("0.0.4538944");
+
+  // If we weren't able to grab it, we should throw a new error
+  if (!myAccountId || !myPrivateKey) {
+    throw new Error(
+      "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present"
+    );
+  }
+
+  // Create our connection to the Hedera network
+  // The Hedera JS SDK makes this really easy!
+  const client = Client.forTestnet();
+  // const client = Client.forMainnet();
+  client.setOperator(myAccountId, myPrivateKey);
+
+  return client;
+};
+
+const deployContract = async (client) => {
+  const contractDeployTx = await new ContractCreateFlow()
+    .setBytecode(bytecode)
+    .setGas(3000000)
+    // .setAdminKey(myPrivateKey)
+    .setStakedAccountId(stakeAccount) // SET THIS ONE...
+    // .setStakedNodeId(3) // OR THIS ONE - DON'T SET BOTH
+    // .setDeclineStakingReward(noRewardFlag) // MISSING IN SDK V2.17 FOR ContractCreateFlow()
+    // .setInitialBalance(initialBalance)
+    // .setConstructorParameters(constructorParameters)
+    // .setContractMemo(memo)
+    // .setAutoRenewAccountId(autoRenewAccountId)
+    // .setAutoRenewPeriod(autoRenewPeriod)
+    // .setMaxAutomaticTokenAssociations(amount)
+    .execute(client);
+
+  const contractDeployRx = await contractDeployTx.getReceipt(client);
+  const contractId = contractDeployRx.contractId;
+  console.log("contractId", contractId.toString());
+};
+
+const updateContract = async (client) => {
+  const contractUpdateTx = new ContractUpdateTransaction()
+    .setContractId("0.0.3928203")
+    // .setStakedAccountId(stakeAccount) // SET THIS ONE...
+    .setStakedNodeId(3) // OR THIS ONE - DON'T SET BOTH
+    .setDeclineStakingReward(false)
+    // .setAdminKey(adminKey)
+    // .setContractMemo(memo)
+    // .setAutoRenewAccountId(autoRenewAccountId)
+    // .setAutoRenewPeriod(autoRenewPeriod)
+    // .setContractExpirationTime(expirationTime)
+    // .setMaxAutomaticTokenAssociations(amount)
+    .freezeWith(client);
+  const contractUpdateSign = await contractUpdateTx.sign(myPrivateKey);
+  const contractUpdateSubmit = await contractUpdateSign.execute(client);
+  const contractUpdateRx = await contractUpdateSubmit.getReceipt(client);
+
+  console.log("contractUpdateRx.status", contractUpdateRx.status);
+};
+
+const resetAllowances = async (client, accountId) => {
+  const result = await axios.get(
+    `https://testnet.mirrornode.hedera.com/api/v1/accounts/${accountId}/allowances/tokens?limit=100&order=desc`
+  );
+
+  const allowances = result.data.allowances;
+  for (const item of allowances) {
+    // Create the transaction
+    const transaction = new AccountAllowanceApproveTransaction()
+      .approveTokenAllowance(item.token_id, accountId, item.spender, 0)
+      .freezeWith(client);
+
+    //Sign the transaction with the owner account key
+    const signTx = await transaction.sign(myPrivateKey);
+
+    //Sign the transaction with the client operator private key and submit to a Hedera network
+    const txResponse = await signTx.execute(client);
+
+    //Request the receipt of the transaction
+    const receipt = await txResponse.getReceipt(client);
+
+    //Get the transaction consensus status
+    const transactionStatus = receipt.status;
+
+    console.log(
+      "The transaction consensus status is " + transactionStatus.toString()
+    );
+  }
+};
+
+const getContractInfo = async (client) => {
+  const accountInfo = await new ContractInfoQuery()
+    .setContractId(contractId)
+    .execute(client);
+  console.log(`- Staking info:`);
+  console.log(`-- stakedAccountId: ${accountInfo.stakingInfo.stakedAccountId}`);
+  console.log(`-- stakedNodeId: ${accountInfo.stakingInfo.stakedNodeId}`);
+  console.log(
+    `-- declineStakingReward: ${accountInfo.stakingInfo.declineStakingReward}`
+  );
+  // https://hashscan.io/testnet/contract/0.0.3934435
+  // -- stakedAccountId: null
+  // -- stakedNodeId: 3
+  // -- declineStakingReward: falsе
+  // https://hashscan.io/testnet/contract/0.0.3934439
+  // -- stakedAccountId: 0.0.7028
+  // -- stakedNodeId: null
+  // -- declineStakingReward: false
+};
+
+const depositHBAR = async (client, contractId) => {
+  const wrappTrans = new ContractExecuteTransaction()
+    //Set the ID of the contract
+    .setContractId(contractId)
+    //Set the gas for the contract call
+    .setGas(3000000)
+    //Amount of HBAR we want to provide
+    .setPayableAmount(1000)
+    //Set the contract function to call
+    .setFunction("deposit", new ContractFunctionParameters())
+    .freezeWith(client);
+  const contractWrapSign = await wrappTrans.sign(myPrivateKey);
+  const contractWrapSubmit = await contractWrapSign.execute(client);
+  const contractWrapRx = await contractWrapSubmit.getReceipt(client);
+  console.log("contractWrapRx.status", contractWrapRx.status);
+};
+
+// Init
+const client = getClient();
